a8065791f14d8aabb0481f85528bee4ef3aaec20
Đỗ Hoàng Trọng <105576582+Dohoangtrong@users.noreply.github.com>
2025-09-20 11:55:39 +0700

    first commit

---
+++ src/test/java/ptit/com/enghub/EngHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ptit.com.enghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

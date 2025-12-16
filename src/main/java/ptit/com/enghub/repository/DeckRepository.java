ae18a35e087c2d4e3ff4f78d0b81c22cb4335dc0
Hieu Nguyen Minh <hieu2003fca2@gmail.com>
2025-12-16 21:02:41 +0700

    update flashcard

--- src/main/java/ptit/com/enghub/repository/DeckRepository.java
+++ src/main/java/ptit/com/enghub/repository/DeckRepository.java
@@ -2,6 +2,7 @@ package ptit.com.enghub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ptit.com.enghub.entity.Deck;
 
@@ -16,11 +17,18 @@ public interface DeckRepository extends JpaRepository<Deck, Long> {
     // 2. Lấy Deck hệ thống NHƯNG trừ những cái User đã Clone rồi
     // Logic: Lấy Deck Public mà ID của nó KHÔNG nằm trong danh sách sourceDeckId mà user đang sở hữu
     @Query("""
-        SELECT d FROM Deck d
-        WHERE d.creatorId = 1
-        AND d.id NOT IN (
-            SELECT my.sourceDeckId FROM Deck my WHERE my.ownerId = :userId AND my.sourceDeckId IS NOT NULL
-        )
+    SELECT d FROM Deck d
+    WHERE d.creatorId = 1
+      AND d.ownerId = 1
+      AND d.sourceDeckId IS NULL
+      AND d.id NOT IN (
+          SELECT c.sourceDeckId
+          FROM Deck c
+          WHERE c.ownerId = :userId
+            AND c.sourceDeckId IS NOT NULL
+      )
     """)
-    List<Deck> findAvailableSystemDecksForUser(Long userId);
+    List<Deck> findAvailableSystemDecksForUser(
+            @Param("userId") Long userId
+    );
 }
